378e5284cb551c524458127d57c502a169a4ee89
SeungBin Kim <luxorienskim@gmail.com>
2023-12-21 13:20:28 +0900

    feat: Add meeting entity

---
+++ libs/entity/src/meetings/meeting.entity.ts
@@ -0,0 +1,26 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm'
+import { Length, Max, Min } from 'class-validator'
+
+@Entity('meetings')
+export class Meeting {
+	@PrimaryGeneratedColumn('increment')
+	id: number
+
+	@Column()
+	@Length(4, 4, { message: 'Year must be 4-digit string' })
+	year: string
+
+	@Column({ type: 'integer' })
+	@Min(1, { message: 'Week must be greater than 0' })
+	@Max(53, { message: 'Week must be less than or equal to 53' })
+	week: number
+
+	@Column({ type: 'date' })
+	date: Date
+
+	@Column({ default: false })
+	canceled: boolean
+
+	@Column()
+	note: string
+}
