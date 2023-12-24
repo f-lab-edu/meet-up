6bc5ce8eeb37a2dd4721d878b029a710101253af
SeungBin Kim <luxorienskim@gmail.com>
2023-12-24 15:30:50 +0900

    test: Add repository providers to each service

--- apps/attendances/src/attendances.controller.spec.ts
+++ apps/attendances/src/attendances.controller.spec.ts
@@ -1,14 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { AttendancesController } from './attendances.controller'
 import { AttendancesService } from './attendances.service'
+import { Repository } from 'typeorm'
+import { getRepositoryToken } from '@nestjs/typeorm'
+import { Attendance } from '@app/entities/attendances/attendance.entity'
 
+type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>
+const createMockRepository = <T = any>(): MockRepository<T> => ({})
 describe('AttendancesController', () => {
 	let attendancesController: AttendancesController
 
 	beforeEach(async () => {
 		const app: TestingModule = await Test.createTestingModule({
 			controllers: [AttendancesController],
-			providers: [AttendancesService],
+			providers: [
+				AttendancesService,
+				{
+					provide: getRepositoryToken(Attendance),
+					useValue: createMockRepository(),
+				},
+			],
 		}).compile()
 
 		attendancesController = app.get<AttendancesController>(AttendancesController)
--- apps/members/src/members.controller.spec.ts
+++ apps/members/src/members.controller.spec.ts
@@ -1,14 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { MembersController } from './members.controller'
 import { MembersService } from './members.service'
+import { Repository } from 'typeorm'
+import { getRepositoryToken } from '@nestjs/typeorm'
+import { Member } from '@app/entities/members/member.entity'
 
+type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>
+const createMockRepository = <T = any>(): MockRepository<T> => ({})
 describe('MembersController', () => {
 	let membersController: MembersController
 
 	beforeEach(async () => {
 		const app: TestingModule = await Test.createTestingModule({
 			controllers: [MembersController],
-			providers: [MembersService],
+			providers: [
+				MembersService,
+				{
+					provide: getRepositoryToken(Member),
+					useValue: createMockRepository(),
+				},
+			],
 		}).compile()
 
 		membersController = app.get<MembersController>(MembersController)
