e09dbb1345767681036c7ecec5ea334b44f93f72
SeungBin Kim <luxorienskim@gmail.com>
2024-01-09 13:05:45 +0900

    feat: Implement basic logging
    Add log consisting HTTP method, original URL, status code, content length, user agent, ip, and trace id

--- libs/log/src/logging.middleware.ts
+++ libs/log/src/logging.middleware.ts
@@ -1,14 +1,23 @@
 import { Injectable, Logger, NestMiddleware } from '@nestjs/common'
 import { NextFunction, Request, Response } from 'express'
+import { randomUUID } from 'crypto'
 
 @Injectable()
 export class LoggingMiddleware implements NestMiddleware {
 	private readonly logger = new Logger('HTTP', { timestamp: true })
 
 	use(req: Request, res: Response, next: NextFunction): void {
+		const { method, originalUrl, ip } = req
+		const userAgent = req.get('user-agent') ?? ''
+		const traceId = randomUUID()
+
 		res.on('finish', () => {
-			this.logger.log('Hello from Logger')
+			const { statusCode } = res
+			const contentLength = res.get('content-length')
+
+			this.logger.log(`${method} ${originalUrl} ${statusCode} ${contentLength} - ${userAgent} ${ip} ${traceId}`)
 		})
+
 		next()
 	}
 }
