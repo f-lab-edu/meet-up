8b800c8075485e46447ea3ef60f4dc33bfd8cb8b
SeungBin Kim <luxorienskim@gmail.com>
2024-01-24 09:47:30 +0900

    feat: Add database related configs

--- apps/members/src/members.module.ts
+++ apps/members/src/members.module.ts
@@ -6,23 +6,27 @@ import { Member } from '@app/entities/members/member.entity'
 import { Meeting } from '@app/entities/meetings/meeting.entity'
 import { Attendance } from '@app/entities/attendances/attendance.entity'
 import { SnakeNamingStrategy } from 'typeorm-naming-strategies'
-import { ConfigModule } from '@nestjs/config'
+import { ConfigModule, ConfigService } from '@nestjs/config'
 import configuration from '@app/config/configuration'
 import { LoggingMiddleware } from '@app/log/logging.middleware'
 
 @Module({
 	imports: [
 		ConfigModule.forRoot({ load: [configuration], ignoreEnvFile: true }),
-		TypeOrmModule.forRoot({
-			type: 'postgres',
-			host: 'database',
-			port: 5432,
-			username: 'postgres',
-			password: 'postgres',
-			database: 'postgres',
-			entities: [Meeting, Member, Attendance],
-			synchronize: true,
-			namingStrategy: new SnakeNamingStrategy(),
+		TypeOrmModule.forRootAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: async (configService: ConfigService) => ({
+				type: 'postgres',
+				host: configService.get<string>('databaseHost'),
+				port: configService.get<number>('databasePort'),
+				username: 'postgres',
+				password: 'postgres',
+				database: 'postgres',
+				entities: [Meeting, Member, Attendance],
+				synchronize: true,
+				namingStrategy: new SnakeNamingStrategy(),
+			}),
 		}),
 		TypeOrmModule.forFeature([Member]),
 	],
