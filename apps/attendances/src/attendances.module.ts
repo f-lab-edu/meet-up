082541d1886def8ec0dccdd1379aac97e2d24739
SeungBin Kim <luxorienskim@gmail.com>
2024-01-29 16:26:17 +0900

    remove: Old configuration file

--- apps/attendances/src/attendances.module.ts
+++ apps/attendances/src/attendances.module.ts
@@ -7,14 +7,13 @@ import { Member } from '@app/entities/members/member.entity'
 import { Attendance } from '@app/entities/attendances/attendance.entity'
 import { SnakeNamingStrategy } from 'typeorm-naming-strategies'
 import { ConfigModule, ConfigService, ConfigType } from '@nestjs/config'
-import configuration from '@app/config/configuration'
 import { LoggingMiddleware } from '@app/log/logging.middleware'
 import { KebabToCamelConversionMiddleware } from '@app/middlewares'
 import databaseConfig from '@app/config/database.config'
 
 @Module({
 	imports: [
-		ConfigModule.forRoot({ load: [configuration], ignoreEnvFile: true }),
+		ConfigModule.forRoot({ ignoreEnvFile: true }),
 		TypeOrmModule.forRootAsync({
 			imports: [ConfigModule],
 			inject: [ConfigService],
