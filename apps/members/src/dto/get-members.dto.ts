f064d20c6b95ac056ea016f1fb53c7a6f0da61ae
SeungBin Kim <luxorienskim@gmail.com>
2024-01-31 23:52:07 +0900

    feat: Update GetMembersDto to compare created-after and created-before for validity

--- apps/members/src/dto/get-members.dto.ts
+++ apps/members/src/dto/get-members.dto.ts
@@ -1,6 +1,7 @@
-import { IsDateString, IsOptional } from 'class-validator'
+import { IsDateString, IsOptional, Validate, ValidationArguments, ValidatorConstraint, ValidatorConstraintInterface } from 'class-validator'
 import { Transform } from 'class-transformer'
 import { MemberDto } from './member.dto'
+import { DeveloperErrorException } from '@app/exceptions'
 
 export type DateFilter = {
 	createdAfter?: Date
@@ -9,10 +10,22 @@ export type DateFilter = {
 
 const toDate = ({ value }) => (value ? new Date(value) : value)
 
+@ValidatorConstraint({ name: 'IsBefore', async: false })
+class IsBeforeConstraint implements ValidatorConstraintInterface {
+	validate(value: any, validationArguments?: ValidationArguments): boolean {
+		const [targetKey] = validationArguments.constraints
+		if (!(targetKey in validationArguments.object))
+			throw new DeveloperErrorException(`Validation failed: ${targetKey} does not exist in the validated class.`)
+		const targetValue = validationArguments.object[targetKey]
+		return targetValue < value
+	}
+}
+
 export class GetMembersDto extends MemberDto implements DateFilter {
 	@IsOptional()
 	@IsDateString()
 	@Transform(toDate)
+	@Validate(IsBeforeConstraint, ['createdBefore'], { message: 'The "created-after" date must be earlier than "created-before" date' })
 	createdAfter?: Date
 
 	@IsOptional()
---
+++ libs/exceptions/src/index.ts
@@ -0,0 +1,6 @@
+export * from './custom-error.exception'
+export * from './developer-error.exception'
+export * from './duplicate-member.exception'
+export * from './member-not-found.exception'
+export * from './member-redundant-deletion.exception'
+export * from './non-sequential-role-update.exception'
